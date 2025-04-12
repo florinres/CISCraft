c4dbe124ccfaffeb04175bd7a103e2a2bb30cb35
Andrei Saldorfean <79752722+AndreiSaldorfean@users.noreply.github.com>
2025-04-12 22:34:10 +0300

    feature: Add assembler's core logic
    	- Remove unused class
    	- Remove unused Tests project
    
    Signed-off-by: Andrei Saldorfean <79752722+AndreiSaldorfean@users.noreply.github.com>

---
+++ Assembler.Business/Assembler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Irony.Parsing;
+
+namespace Assembler.Business
+{
+    public class Assembler
+    {
+        Encoder encoder;
+        CiscGrammar grammar;
+        Parser parser;
+        public Assembler()
+        {
+            encoder = new Encoder();
+            grammar = new CiscGrammar();
+            parser  = new Parser(grammar);
+        }
+        public ushort[] Assemble(string sourceCode)
+        {
+            var tree = parser.Parse(sourceCode);
+
+            if (tree.HasErrors())
+            {
+                foreach (var err in tree.ParserMessages)
+                {
+                    Console.WriteLine($"Error: {err.Message} at {err.Location}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parsing successful.");
+            }
+
+            Console.WriteLine("AST Traversal:");
+            return encoder.Encode(tree.Root);
+        }
+
+    }
+}
--- Assembler.Main/Program.cs
+++ Assembler.Main/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Irony.Parsing;
+
+namespace Assembler.Business
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "main.s";
+            string file;
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    file = reader.ReadToEnd();
+                    Assembler assembler = new Assembler();
+                    assembler.Assemble(file);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"Error: File not found - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+}
