269e6f91c6581dfcfaed3340809b699a72b311ee
AVRAM MATEI-ALEXANDRU <113266427+Alex552-stack@users.noreply.github.com>
2025-04-25 20:28:16 +0300

    Fixed Incorrect DependencyProperty owner type

--- Ui/Views/UserControls/Diagram/AroundTheEdgeBus.xaml.cs
+++ Ui/Views/UserControls/Diagram/AroundTheEdgeBus.xaml.cs
@@ -7,13 +7,13 @@ namespace Ui.Views.UserControls.Diagram;
 public partial class AroundTheEdgeBus : UserControl
 {
     public static readonly DependencyProperty WrapWidthProperty =
-        DependencyProperty.Register(nameof(WrapWidth), typeof(double), typeof(StraightLineBus), new PropertyMetadata(300.0));
+        DependencyProperty.Register(nameof(WrapWidth), typeof(double), typeof(AroundTheEdgeBus), new PropertyMetadata(300.0));
 
     public static readonly DependencyProperty WrapHeightProperty =
-        DependencyProperty.Register(nameof(WrapHeight), typeof(double), typeof(StraightLineBus), new PropertyMetadata(300.0));
+        DependencyProperty.Register(nameof(WrapHeight), typeof(double), typeof(AroundTheEdgeBus), new PropertyMetadata(300.0));
     
     public static readonly DependencyProperty LineThicknessProperty =
-        DependencyProperty.Register(nameof(LineThickness), typeof(double), typeof(StraightLineBus), new PropertyMetadata(6.0));
+        DependencyProperty.Register(nameof(LineThickness), typeof(double), typeof(AroundTheEdgeBus), new PropertyMetadata(6.0));
 
     public double WrapWidth 
     {
