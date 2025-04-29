d1d50b749d7ceb7c185cb8f093e8a50f50c9dfcb
Mihai Neag <mihaineag6@gmail.com>
2025-04-30 01:22:00 +0300

    feature: Added basic structure for MicroProgramMemory

---
+++ MicroprogramMemory.Business/MicroProgramMemory.cs
@@ -0,0 +1,28 @@
+﻿namespace MicroprogramMemory.Business
+{
+    public class MicroProgramMemory
+    {
+        Dictionary<string,ulong> microMemoryDump;
+
+        private static MicroProgramMemory? microMemoryInstance;
+        private static readonly Lock _lock = new();
+
+        private MicroProgramMemory()
+        {
+            this.microMemoryDump = new Dictionary<string, ulong>();
+        }
+
+        public static MicroProgramMemory GetMicroMemoryInstance()
+        {
+            lock (_lock) // ensure that only one thread can access this portion of code
+            {
+                if (microMemoryInstance == null)
+                {
+
+                    microMemoryInstance = new MicroProgramMemory();
+                }
+            }
+            return microMemoryInstance;
+        }
+    }
+}
---
+++ Ui/Services/MicroProgramMemoryService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ui.Interfaces;
+using MicroMemory = MicroprogramMemory.Business.MicroProgramMemory;
+
+namespace Ui.Services
+{
+    class MicroProgramMemoryService : IMicroProgramMemory
+    {
+        readonly MicroMemory uMemory;
+        string microMemoryFile;
+
+        public MicroProgramMemoryService()
+        {
+            this.uMemory = MicroMemory.GetMicroMemoryInstance();
+            this.microMemoryFile = Path.Combine(AppContext.BaseDirectory, "microcode.json");
+        }
+        public void EditMicroRoutine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, ulong> GetMicroProgramMemory()
+        {
+            throw new NotImplementedException();
+        }
+        public void LoadMicroProgram()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
