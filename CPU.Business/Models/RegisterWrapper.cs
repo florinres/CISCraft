14835450a4536ef6eabdaa97ec5149c4e80dabde
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-05-20 11:40:57 -0700

    feature: Update register wrapper class
    Signed-off-by: Andrei Saldorfean <andreisaldorfean@gmail.com>

---
+++ CPU.Business/Models/RegisterWrapper.cs
@@ -0,0 +1,48 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CPU.Business.Models;
+
+public class RegisterWrapper : ObservableObject
+{
+    private short[] _registers { get; }
+    private short[] _gpr { get; }
+
+    public RegisterWrapper(int amount)
+    {
+        _registers = new short[amount];
+        _gpr = new short[16];
+    }
+    public RegisterWrapper()
+    {
+        _registers = new short[23];
+        _gpr = new short[16];
+    }
+
+    public short this[int index]
+    {
+        get => _registers[index];
+        set
+        {
+            if (_registers[index] == value) return;
+            
+            _registers[index] = value;
+            OnPropertyChanged($"Item[{index}]");
+            OnPropertyChanged(nameof(_registers));
+        }
+    }
+    
+    public short this[REGISTERS index]
+    {
+        get => _registers[(int)index];
+        set
+        {
+            if (_registers[(int)index] == value) return;
+            
+            _registers[(int)index] = value;
+            OnPropertyChanged($"Item[{index}]");
+            OnPropertyChanged(nameof(_registers));
+        }
+    }
+    public short[] Registers => _registers;
+    public short[] Gpr => _gpr;
+}
