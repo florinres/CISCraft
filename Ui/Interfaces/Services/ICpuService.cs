9bdd3b503fa246477e257b9794296e835e58ddbc
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-08-16 01:02:54 -0700

    feature: Add dummy highlight and control debugging buttons

--- Ui/Interfaces/Services/ICpuService.cs
+++ Ui/Interfaces/Services/ICpuService.cs
@@ -1,7 +1,14 @@
+using Ui.ViewModels.Generics;
+
 namespace Ui.Interfaces.Services;
 
 public interface ICpuService
 {
+    HighlightCurrentLineBackgroundRenderer? Highlight { get; set; }
     Task LoadJsonMpm(string filePath = "", bool debug = false);
     (int, int) StepMicrocommand();
+    public void SetActiveEditor(FileViewModel fileViewModel);
+    public void ResetProgram();
+    public void StartDebugging();
+    public void StopDebugging();
 }
