068ad14adc4853d64f346fe34d582ab8aa15d2b3
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-08-17 22:23:55 +0300

    fix: Use MPM from configs for the ui application
    Signed-off-by: Andrei Saldorfean <andreisaldorfean@gmail.com>

--- Ui/Services/CpuService.cs
+++ Ui/Services/CpuService.cs
@@ -52,8 +52,8 @@ public class CpuService : ICpuService
         
         if (filePath == "")
         {
-            var projectRoot = AppContext.BaseDirectory!;
-            fullPath = Path.Combine(projectRoot, "Assets", "Mpm.json");
+            var projectRoot = AppContext.BaseDirectory + "../../../";
+            fullPath = Path.Combine(projectRoot, "../Configs", "Mpm.json");
         }
         else
         {
