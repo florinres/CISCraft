dc4049c773246706f3f6d0d5f830a45d89742c62
Mihai Neag <mihaineag6@gmail.com>
2025-09-25 00:01:43 +0300

    fix: Cut offset to 8 bits

--- Assembler.Business/Encoder.cs
+++ Assembler.Business/Encoder.cs
@@ -557,7 +557,7 @@ namespace Assembler.Business
                         Debug.WriteLine(" " + node.Token.Value);
                         Debug.WriteLine(_instrAddress + 2 + " " + node.Token.Value);
                     }
-                        _instructionParts.Offset2 = (uint)(Convert.ToInt32(node.Token.Value) & 0xFF); // the only the 8 bit value
+                        _instructionParts.Offset2 = (uint)(Convert.ToInt32(node.Token.Value) & 0xFF); // take the only the 8 bit value
                     _instrAddress += 4;
                     return;
                 }
@@ -593,7 +593,8 @@ namespace Assembler.Business
                     {
                         if(_debug) Console.Write(" " + node.ChildNodes[0].Token.Value);
                         am = 0b11;
-                        offset = Convert.ToUInt32(node.ChildNodes[0].Token.Value);
+
+                        offset = (uint)(Convert.ToInt32(node.ChildNodes[0].Token.Value) & 0xFF); // take the only the 8 bit value
                         _instrAddress += 2;
                     }
                     return;
