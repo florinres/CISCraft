47c79ba21777e7e8b824f886d5505741406ad454
MN3141 <mihai.neag@ulbsibiu.ro>
2025-09-25 16:15:23 +0300

    feature: Added logger for code assembly and message box

--- Assembler.Business/Assembler.cs
+++ Assembler.Business/Assembler.cs
@@ -7,6 +7,9 @@ namespace Assembler.Business
         readonly Encoder Encoder;
         readonly CiscGrammar Grammar;
         readonly Parser Parser;
+        LogWriter _logWriter;
+        private string _logsDir;
+        private string _parserLog;
         public static ushort ProgramStartingAddress
         {
             get
@@ -24,6 +27,9 @@ namespace Assembler.Business
             Encoder = new Encoder();
             Grammar = new CiscGrammar();
             Parser  = new Parser(Grammar);
+            _logWriter = new LogWriter();
+            _logsDir = Path.GetFullPath(AppContext.BaseDirectory + "../../../../Logs");
+            _parserLog = "parser.log";
         }
         public byte[] Assemble(string sourceCode, out int len)
         {
@@ -31,17 +37,16 @@ namespace Assembler.Business
 
             if (tree.HasErrors())
             {
+                Dictionary<string,string> parsingErrors = new Dictionary<string,string>();
                 foreach (var err in tree.ParserMessages)
-                {
-                    Console.WriteLine($"Error: {err.Message} at {err.Location}");
-                }
+                    parsingErrors[err.Message] = err.Location.ToString();
+                LogParsingInfo(parsingErrors);
                 len = 0;
                 return new byte[0];
             }
             else
-            {
-                Console.WriteLine("Parsing successful.");
-            }
+                LogParsingInfo(new Dictionary<string, string> { { "STATUS:", "Parsing successful." } });
+
             return Encoder.Encode(tree.Root, out len);
         }
         public Dictionary<ushort /* pc */, ushort /* line number */> GetDebugSymbols(ushort pcOffset)
@@ -50,6 +55,18 @@ namespace Assembler.Business
                     kvp => (ushort)(kvp.Key + pcOffset),
                     kvp => kvp.Value
                 );
+        }
+
+        private void LogParsingInfo(Dictionary<string,string> msg)
+        {
+            string logPath = _logsDir + "\\" + _parserLog;
+            Directory.CreateDirectory(_logsDir);
+            File.Create(logPath).Dispose();
+            string formatedMsg = DateTime.Now.ToString() + "\n{\n" +
+            string.Join(",\n", msg.Select(kvp => $"  \"{kvp.Key}\": \"{kvp.Value}\"")) + 
+            "\n}";
+
+            _logWriter.LogInfo(formatedMsg, logPath, WriteMode.Overwrite);
         }
     }
 }
---
+++ Assembler.Business/LogWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Assembler.Business
+{
+    public enum WriteMode
+    {
+        Append,
+        Overwrite
+    }
+
+    public class LogWriter
+    {
+        public LogWriter() { }
+        public void LogInfo(string message, string filePath, WriteMode mode = WriteMode.Append)
+        {
+            try
+            {
+                switch (mode)
+                {
+                    case WriteMode.Append:
+                        File.AppendAllText(filePath, message + Environment.NewLine);
+                        break;
+                    case WriteMode.Overwrite:
+                        File.WriteAllText(filePath, message + Environment.NewLine);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to file LogWriter.cs: {ex.Message}");
+                Debug.WriteLine($"Error writing to file LogWriter.cs: {ex.Message}");
+            }
+        }
+    }
+}
